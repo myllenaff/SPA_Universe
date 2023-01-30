f62e0a650376b62473ad69d59aca4a828842273c
Myllena Freitas <myllenaff@gmail.com>
2023-01-30 18:49:06 +0000

    Fix bug na navegação e refactor na troca de classes

--- js/router.js
+++ js/router.js
@@ -1,28 +1,37 @@
-export class Router {
+import { changeClassOnHandle } from './index.js'
 
+export class Router {
   routes = {}
 
   add(routeName, page) {
     this.routes[routeName] = page
   }
-  
+
   route(event) {
     event = event || window.event
+
     event.preventDefault()
 
-    window.history.pushState({}, "", event.target.href)
+    window.history.pushState({}, '', event.target.href)
 
     this.handle()
   }
-   handle() {
+  handle() {
     const { hash } = window.location
-    const route = this.routes[hash] || this.routes[404]
+
+    let route
+    if (!hash) {
+      window.location = '#home'
+      route = this.routes['#home']
+    } else {
+      route = this.routes[hash] || this.routes[404]
+    }
 
     fetch(route)
-    .then(data => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-    })
-   }
+      .then(data => data.text())
+      .then(html => {
+        document.querySelector('#app').innerHTML = html
+        changeClassOnHandle(hash || '#home')
+      })
   }
-
+}
